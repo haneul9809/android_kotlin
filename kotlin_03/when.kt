cbd826677108886c99824787498695775e9cd2ba
haneul9809 <67767912+haneul9809@users.noreply.github.com>
2023-01-30 19:32:06 +0900

    Create when.kt

---
+++ kotlin_03/when.kt
@@ -0,0 +1,10 @@
+fun main() {
+    var data = 10
+    when(data){
+        10 -> println("data is 10")
+        20 -> println("data is 20")
+        else -> {
+            println("data is not valid data")
+        }
+    }
+}
