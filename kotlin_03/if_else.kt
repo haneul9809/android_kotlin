4533e7004f2a8bddb74c78958a1dd7161b88860e
haneul9809 <67767912+haneul9809@users.noreply.github.com>
2023-01-30 19:30:05 +0900

    Create if_else.kt

---
+++ kotlin_03/if_else.kt
@@ -0,0 +1,10 @@
+fun main() {
+    var data = 10
+    if (data>10){
+        println("data > 10")
+    } else if (data > 0 && data <= 10){
+        println("data > 0 && data <= 10")
+    } else {
+        println("data <= 0")
+    }
+}
