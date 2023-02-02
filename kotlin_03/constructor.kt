fca73846a00de318f1e5e935d119f1f078b4b0ca
haneul9809 <67767912+haneul9809@users.noreply.github.com>
2023-02-02 10:04:55 +0900

    Create constructor.kt

---
+++ kotlin_03/constructor.kt
@@ -0,0 +1,10 @@
+class User {
+    var name = "haneul"
+    constructor(name: String){
+        this.name = name
+    }
+    fun someFun() {
+        println("name : $name")
+    }
+    class SomeClass(){}
+}
